8ec210c203e50cec8d775e0e3f61971333cf91c0
T-uulik <tonis.tuulik@tktk.ee>
2023-04-07 15:57:25 +0300

    Week 5

---
+++ AND_Operator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter your Math result:");
+int math, bio, chem;
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 && bio >= 90 && chem >= 90)
+{
+    Console.WriteLine("Congratulations! You've been accepted!");
+}
+else
+{
+    Console.WriteLine("You didn't qualify for enrollment.");
+}
+Console.WriteLine("Have a nice day!");
---
+++ OR_Operator/Program.cs
@@ -0,0 +1,22 @@
+﻿// math >= 90 OR chem >= 90 OR bio >= 90
+
+int math, bio, chem;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && bio  >= 90) || (math >= 90 && chem >= 90) || (chem >= 90 && bio >= 90))
+    {
+        Console.WriteLine("Congratulations! You've been accepted!");
+    }
+    else
+    {
+        Console.WriteLine("You didn't qualify for enrollment.");
+    }
+Console.WriteLine("Have a nice day!");
